9d504cf92cff1508251b1a71b7329a75bd290b1e
Ian McKenzie <imckenzie777@gmail.com>
2020-02-14 20:22:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ var greetSpecialGuest = "Hello " + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `The conversation will be on ${topic}`;
+var conversation = `Let\'s talk about ${topic}.`;
